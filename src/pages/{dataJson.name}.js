02d001ec485647ba383ea732b70071394f76b6f5
tomek <tomasz.kuncio@gmail.com>
2023-04-26 18:47:30 +0200

    Made some refactorization

--- src/pages/{dataJson.name}.js
+++ src/pages/{dataJson.name}.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Header from '../components/Header';
 import viewFullIcon from '../assets/images/icons/icon-view-image.svg';
-import Modal from 'react-modal';
-
-const customStyles = {
-	content: {
-		top: '0',
-		left: '0',
-		width: '100vw',
-		height: '100vh',
-		backgroundColor: 'rgba(0, 0, 0, 0.8)',
-		zIndex: '11',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-};
-Modal.setAppElement('#___gatsby');
+import ModalSinglePicture from '../components/ModalSinglePicture';
 
 const Picture = ({ data }) => {
 	const [modalIsOpen, setIsOpen] = useState(false);
-	const modalBox = useRef(null);
 
 	function openModal() {
 		setIsOpen(true);
@@ -32,48 +16,19 @@ const Picture = ({ data }) => {
 		setIsOpen(false);
 	}
 
-	useEffect(() => {
-		let element
-		const listener = (e) => {
-			if (e.target.nodeName !== 'IMG') closeModal();
-		};
-		if (modalIsOpen) {
-			element = modalBox.current.node;
-			element.addEventListener('click', listener);
-		}
-		return () => {
-			if (element) {
-				element.removeEventListener('click', listener);
-			}
-		};
-	}, [modalIsOpen]);
-
 	return (
 		<>
 			<Header isSlideShow={true} />
 			<main className='mt-5 grid grid-cols-1 gap-10 lg:container md:mt-12 lg:mx-auto lg:grid-cols-2'>
-				<Modal
-					isOpen={modalIsOpen}
-					onRequestClose={closeModal}
-					style={customStyles}
-					contentLabel='Example Modal'
-					ref={modalBox}>
-					<div className='relative z-50 h-3/4'>
-						<GatsbyImage
-							image={getImage(
-								data.dataJson.images.hero.large.childrenImageSharp[0]
-									.gatsbyImageData
-							)}
-							alt={data.dataJson.name}
-							className='modal-box flex flex-row justify-center'
-						/>
-						<button
-							onClick={closeModal}
-							className='absolute -top-8 right-0 tracking-widest text-white transition-transform hover:text-lg'>
-							CLOSE
-						</button>
-					</div>
-				</Modal>
+				<ModalSinglePicture
+					modalIsOpen={modalIsOpen}
+					closeModal={closeModal}
+					image={
+						data.dataJson.images.hero.large.childrenImageSharp[0]
+							.gatsbyImageData
+					}
+					imgAlt={data.dataJson.name}
+				/>
 				<div className='relative z-0'>
 					<div className='z-10 bg-white p-5 tracking-wide md:absolute md:left-2/3 md:top-0 md:ml-0 lg:left-2/3'>
 						<p className='text-4xl font-bold lg:text-2xl xl:text-5xl'>
@@ -95,7 +50,7 @@ const Picture = ({ data }) => {
 						<button
 							onClick={openModal}
 							className='absolute bottom-5 left-0 ml-10 flex appearance-none flex-row items-center rounded-sm bg-hero-background px-4 py-2 text-xs tracking-wide text-white transition-colors hover:bg-hero-hover focus:outline-none md:ml-24'>
-							<img src={viewFullIcon} className='mr-3' /> VIEW FULL
+							<img src={viewFullIcon} className='mr-3' alt=''/> VIEW FULL
 						</button>
 					</div>
 
